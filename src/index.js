dc1c21ac8f155dbb15e6ea74def1bc45339de249
Alex Borisenkov <baster84@mail.ru>
2020-10-24 22:59:47 +1000

    feat: add check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (let i=0; i < str.length; i++)
+  {
+    const char = str[i];
+    let open = 0;
+    let num = -1;
+    for (let j=0; j < bracketsConfig.length; j++){
+      if (char === bracketsConfig[j][0] && bracketsConfig[j][0] === bracketsConfig[j][1]){
+        open = 0;
+        num = j;
+        break;
+      }
+      if (char === bracketsConfig[j][0]){
+        open = 1;
+        num = j;
+        break;
+      }
+      if (char === bracketsConfig[j][1]){
+        open = 2;
+        num = j;
+        break;
+      }
+    }
+    // символ не является скобкой из набора
+    if (num < 0) continue;
+    // если скобка открывается, то добавляем её номер в стек
+    if (open == 1){
+      stack.push(num);
+      continue;
+    }
+    // если скобка закрывается, то проверяем её пару в стеке
+    if (open == 2) {
+      if (stack.length === 0 || stack.pop() !== num){
+        return false;
+      }
+    }
+    // если скобка вида |
+    if (open === 0) {
+      if (num !== stack[stack.length - 1]){
+        stack.push(num);
+      } else {
+        stack.pop();
+      }
+    }
+  }
+  return stack.length === 0;
 }
